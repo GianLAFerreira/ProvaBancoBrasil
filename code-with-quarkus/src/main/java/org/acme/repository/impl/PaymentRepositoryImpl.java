61259581aa64b4fcd7cd227ac6474478f3df78cc
Gian Lucas Alves Ferreira <78167245+GianLAFerreira@users.noreply.github.com>
2024-08-07 17:57:01 -0300

    ajuste

--- code-with-quarkus/src/main/java/org/acme/repository/impl/PaymentRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package org.acme.repository.impl;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-import org.acme.entity.Payment;
-
-@ApplicationScoped
-public class PaymentRepositoryImpl implements PanacheRepository<Payment> {
-}
